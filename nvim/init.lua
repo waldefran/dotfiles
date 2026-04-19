ac8c9cbd0c2c2b43083f7c4bd0c821b49142290c
waldefran <valdefranpereira@gmail.com>
2026-04-19 12:40:16 -0300

    chore: update nvim config with user preferences and add wezterm catppuccin theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,132 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- ===============================================
+-- Neovim configuration (Neovim 0.10.x)
+-- ===============================================
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- Theme
+  { "folke/tokyonight.nvim" },
+
+  -- Status line
+  { "nvim-lualine/lualine.nvim" },
+
+  -- Git integration
+  { "lewis6991/gitsigns.nvim" },
+
+  -- Mini utilities (pairs, surround, etc.)
+  { "echasnovski/mini.nvim", version = "*" },
+
+  -- Treesitter (syntax highlighting)
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+  -- Fuzzy finder (uses builtin fzf-java for now)
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    cond = function() return vim.fn.executable("fzf") == 1 end,
+  },
+}, {
+  defaults = { lazy = false },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip", "tarPlugin", "netrwPlugin",
+        "rplugin", "spellfilePlugin", "tutor", "quotesPermutations",
+      },
+    },
+  },
+})
+
+-- ===============================================
+-- Options
+-- ===============================================
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.colorcolumn = "100"
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+
+opt.termguicolors = true
+opt.hidden = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
+opt.updatetime = 200
+opt.timeoutlen = 300
+opt.splitright = true
+opt.splitbelow = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.wrap = false
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.iskeyword:append("-")
+
+-- Better netrw
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = 25
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
+})
+
+-- ===============================================
+-- Keymaps
+-- ===============================================
+
+vim.g.mapleader = " "
+local map = vim.keymap.set
+
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Window left" })
+map("n", "<C-j>", "<C-w>j", { desc = "Window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Window up" })
+map("n", "<C-l>", "<C-w>l", { desc = "Window right" })
+
+-- Resize
+map("n", "<M-h>", "<C-w>5<", { desc = "Resize left" })
+map("n", "<M-l>", "<C-w>5>", { desc = "Resize right" })
+
+-- Files / buffers
+map("n", "<leader>ee", ":Lexplore<CR>", { desc = "Toggle netrw" })
+map("n", "<leader>ff", ":FzfLua files<CR>", { desc = "Find files (if fzflua installed)" })
+map("n", "<leader>fb", ":buffers<CR>", { desc = "List buffers" })
+
+-- Git
+map("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk" })
+map("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
+
+-- Move lines
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- ===============================================
+-- Theme
+-- ===============================================
+
+vim.cmd([[colorscheme tokyonight]])
+vim.opt.background = "dark"
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,38 +1,61 @@
 local wezterm = require 'wezterm'
+
 return {
-	adjust_window_size_when_changing_font_size = false,
-	color_scheme = 'Catppuccin Mocha',
-	enable_tab_bar = false,
-	font_size = 16.0,
-	font = wezterm.font('JetBrains Mono'),
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
-	window_decorations = 'RESIZE',
-	keys = {
-		{
-			key = 'q',
-			mods = 'CTRL',
-			action = wezterm.action.ToggleFullScreen,
-		},
-		{
-			key = '\'',
-			mods = 'CTRL',
-			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-		},
-	},
-	mouse_bindings = {
-	  -- Ctrl-click will open the link under the mouse cursor
-	  {
-	    event = { Up = { streak = 1, button = 'Left' } },
-	    mods = 'CTRL',
-	    action = wezterm.action.OpenLinkAtMouseCursor,
-	  },
-	},
+  adjust_window_size_when_changing_font_size = false,
+  color_scheme = 'Catppuccin Mocha',
+  color_schemes = {
+    ['Catppuccin Mocha'] = {
+      foreground = '#cdd6f4',
+      background = '#1e1e2e',
+      cursor_border = '#cba6f7',
+      cursor_bg = '#cba6f7',
+      cursor_fg = '#1e1e2e',
+      selection_bg = '#585b70',
+      selection_fg = '#cdd6f4',
+      ansi = {
+        '#45475a', -- black
+        '#f38ba8', -- red
+        '#a6e3a1', -- green
+        '#f9e2af', -- yellow
+        '#89b4fa', -- blue
+        '#cba6f7', -- mauve
+        '#94e2d5', -- teal
+        '#bac2de', -- white
+      },
+      brights = {
+        '#585b70', -- bright black
+        '#f38ba8', -- bright red
+        '#a6e3a1', -- bright green
+        '#f9e2af', -- bright yellow
+        '#89b4fa', -- bright blue
+        '#cba6f7', -- bright mauve
+        '#94e2d5', -- bright teal
+        '#cdd6f4', -- bright white
+      },
+    },
+  },
+  enable_tab_bar = false,
+  font_size = 16.0,
+  font = wezterm.font('JetBrains Mono Nerd Font'),
+  window_background_opacity = 1.0,
+  window_decorations = 'RESIZE',
+  keys = {
+    {
+      key = 'q',
+      mods = 'CTRL',
+      action = wezterm.action.ToggleFullScreen,
+    },
+    {
+      key = '\'',
+      mods = 'CTRL',
+      action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+    },
+  },
+  mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'CTRL',
+      action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+  },
 }
